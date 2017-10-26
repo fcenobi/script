1ff32fcfd032bdf0f8bf7dfdbe8254a6e5a65271
fcenobi <fcenobi@mlsnc.it>
2017-10-26 23:58:29 +0200

    Add files via upload

---
+++ installlamp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt-get remove  mysql-server --purge
+sudo apt-get remove  mysql-client --purge
+sudo apt-get remove  apache2 php5 php5-gd php-xml-parser php5-intl php5-sqlite php5-mysql smbclient curl libcurl3 php5-curl --purge
+
