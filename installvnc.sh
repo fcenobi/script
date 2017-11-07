5eeca9d3ddc5c88f2bfb4209556ec87976ee0b49
fcenobi <fcenobi@mlsnc.it>
2017-11-07 21:38:05 +0100

    Update installvnc.sh

--- installvnc.sh
+++ installvnc.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 wget https://www.realvnc.com/download/file/vnc.files/VNC-Server-6.2.0-Linux-x64.deb
-sudo dpkg -i VNC-Server*
-sudo apt-get install -f
 wget https://www.realvnc.com/download/file/viewer.files/VNC-Viewer-6.17.731-Linux-x64.deb
-sudo dpkg -i VNC-Viewer*
+sudo dpkg -i VNC-*
 sudo apt-get install -f
 sudo systemctl enable vncserver-x11-serviced.service
 sudo systemctl start vncserver-x11-serviced.service
 echo LXSX5-JRR6E-LSK7H-SE36N-EUVKA >> ~/vncserverlicenza.txt
-
+vnclicense -add LXSX5-JRR6E-LSK7H-SE36N-EUVKA
 
 
