a7348f6ab1ff1b1a406597553d6a685ef4a66b7b
fcenobi <fcenobi@mlsnc.it>
2017-11-07 23:12:05 +0100

    Update instwebmin.sh

--- instwebmin.sh
+++ instwebmin.sh
@@ -1,5 +1,4 @@
-echo deb http://download.webmin.com/download/repository sarge contrib >> /etc/apt/source.list.d/webmin.list
-cd /root
+echo deb http://download.webmin.com/download/repository sarge contrib >> /etc/apt/source.list
 wget http://www.webmin.com/jcameron-key.asc
 apt-key add jcameron-key.asc
 apt-get update && apt-get install apt-transport-https && apt-get install -y  webmin
