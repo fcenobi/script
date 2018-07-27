7b12c6a2f2019188270563312b3c35d4493031a4
fcenobi <11414315+fcenobi@users.noreply.github.com>
2018-07-27 03:52:54 +0200

    Update chocoinstapp.cmd

--- chocoinstapp.cmd
+++ chocoinstapp.cmd
@@ -1,2 +1,7 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "cinst -y notepad2 notepadplusplus.install PowerShell putty.install nirlauncher nmap treesizefree Sudo sysinternals 7zip.commandline 7zip.install 7zip.portable choco-package-list-backup chocolatey chocolatey-core.extension chocolatey-uninstall.extension chocolateygui ConEmu Far Firefox flashplayerplugin git git.install gitextensions GoogleChrome hot-chocolatey jre8 kdiff3 mc nimbletext toolsroot winscp.install ActivePerl python3 pip3 swish launchy-beta"
+
+
+ dbatools google-drive-file-stream qemu  megatools filelist viscosity vnc-connect dokany dokany-redistributable 
+ powershell-preview  netdrive
+ winlogbeat  visualstudio2017-powershelltools vdesk  wpd  ericzimmermantools
