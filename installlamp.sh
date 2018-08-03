2fc3922d767dc3b0fa0da23a565ed72fee64920c
fcenobi <11414315+fcenobi@users.noreply.github.com>
2018-08-03 19:30:21 +0200

    Update installlamp.sh

--- installlamp.sh
+++ installlamp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get remove  mysql-server --purge
 sudo apt-get remove  mysql-client --purge
-sudo apt-get remove  apache2 php5 php5-gd php-xml-parser php5-intl php5-sqlite php5-mysql smbclient curl libcurl3 php5-curl --purge
+sudo apt-get remove  apache2 php php-gd php-xml-parser php-intl php-sqlite php-mysql smbclient curl libcurl3 php-curl --purge
 
