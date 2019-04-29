6877d63fe85a3b9df2cfa5a3d59f829d50855aa9
fcenobi <11414315+fcenobi@users.noreply.github.com>
2019-04-29 02:24:37 +0200

    Update createpsprofile.ps1

--- createpsprofile.ps1
+++ createpsprofile.ps1
@@ -4,3 +4,5 @@ if(!(Test-Path -Path $profile.AllUsersAllHosts  )){New-Item -path $profile.AllUs
 if(!(Test-Path -Path $profile.AllUsersCurrentHost  )){New-Item -path $profile.AllUsersCurrentHost -type file -force}
 if(!(Test-Path -Path $profile.CurrentUserAllHosts  )){New-Item -path $profile.CurrentUserAllHosts -type file -force}
 if(!(Test-Path -Path $profile.CurrentUserCurrentHost )){New-Item -path $profile.CurrentUserCurrentHost -type file -force}
+
+
