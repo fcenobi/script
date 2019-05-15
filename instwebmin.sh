9fa469a7633979d435ac3d4cbecc489724d7aa1a
fcenobi <11414315+fcenobi@users.noreply.github.com>
2019-05-15 09:31:00 +0200

    Update instwebmin.sh

--- instwebmin.sh
+++ instwebmin.sh
@@ -1,4 +1,5 @@
-sudo echo deb http://download.webmin.com/download/repository sarge contrib >> /etc/apt/sources.list
+login root
+echo deb http://download.webmin.com/download/repository sarge contrib >> /etc/apt/sources.list
 wget http://www.webmin.com/jcameron-key.asc
 apt-key add jcameron-key.asc
-sudo apt-get update && apt-get install apt-transport-https && apt-get install -y  webmin
+apt-get update && apt-get install apt-transport-https && apt-get install -y  webmin
