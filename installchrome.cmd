36430b9023c0b327cd157a899d8ccc0eb695dbac
fcenobi <11414315+fcenobi@users.noreply.github.com>
2019-08-12 05:01:04 +0200

    Create installchrome.cmd

---
+++ installchrome.cmd
@@ -0,0 +1,1 @@
+PowerShell -Command "& {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/silent /install' -Verb RunAs -Wait; Remove-Item $P}"
