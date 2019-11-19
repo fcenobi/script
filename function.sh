b225f9d469cfe239ce3808ff72c738468e2c0559
fcenobi <11414315+fcenobi@users.noreply.github.com>
2019-11-19 04:34:26 +0100

    Update function.sh

--- function.sh
+++ function.sh
@@ -6,3 +6,9 @@ function lls {
   ls -AHl $LLS_PATH | awk "{k=0;for(i=0;i<=8;i++)k+=((substr(\$1,i+2,1)~/[rwx]/) \
                             *2^(8-i));if(k)printf(\"%0o \",k);print}"
 }
+
+function nocommentxml {
+  XMLFILE=$1
+
+ cat $XMLFILE | sed 's/<!--/\x0<!--/g;s/-->/-->\x0/g' | grep -zv '^<!--' | tr -d '\0' 
+ }
