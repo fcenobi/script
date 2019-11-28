11b6b19761f3022128d735fa16bbf9f8c9c15f7e
fcenobi <11414315+fcenobi@users.noreply.github.com>
2019-11-28 05:13:19 +0100

    Update instwebmin.sh

--- instwebmin.sh
+++ instwebmin.sh
@@ -1,4 +1,4 @@
-login root
+cd ~
 echo deb http://download.webmin.com/download/repository sarge contrib >> /etc/apt/sources.list
 wget http://www.webmin.com/jcameron-key.asc
 apt-key add jcameron-key.asc
