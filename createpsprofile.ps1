31b1ee7c81d01201aa8abf6d6175685c54e15415
Filippo Cenobi <11414315+fcenobi@users.noreply.github.com>
2020-09-14 00:43:59 +0200

    Update createpsprofile.ps1

--- createpsprofile.ps1
+++ createpsprofile.ps1
@@ -4,5 +4,19 @@ if(!(Test-Path -Path $profile.AllUsersAllHosts  )){New-Item -path $profile.AllUs
 if(!(Test-Path -Path $profile.AllUsersCurrentHost  )){New-Item -path $profile.AllUsersCurrentHost -type file -force}
 if(!(Test-Path -Path $profile.CurrentUserAllHosts  )){New-Item -path $profile.CurrentUserAllHosts -type file -force}
 if(!(Test-Path -Path $profile.CurrentUserCurrentHost )){New-Item -path $profile.CurrentUserCurrentHost -type file -force}
-
+Test-Path -Path $profile
+Test-Path -Path $profile.AllUsersAllHosts 
+Test-Path -Path $profile.AllUsersCurrentHost 
+Test-Path -Path $profile.CurrentUserAllHosts 
+Test-Path -Path $profile.CurrentUserCurrentHost 
+echo $profile
+get-content $profile
+echo $profile.AllUsersAllHosts 
+get-content $profile.AllUsersAllHosts 
+echo $profile.AllUsersCurrentHost 
+get-content $profile.AllUsersCurrentHost  
+echo $profile.CurrentUserAllHosts 
+get-content  $profile.CurrentUserAllHosts 
+echo $profile.CurrentUserCurrentHost 
+get-content $profile.CurrentUserCurrentHost 
 
