f65f2bde0ca537595d412ae7fd022482b12cc3d2
Filippo Cenobi <11414315+fcenobi@users.noreply.github.com>
2021-01-24 17:57:17 +0100

    Update ,bash_aliases.sh

--- etc/skel/,bash_aliases.sh
+++ etc/skel/,bash_aliases.sh
@@ -1,3 +1,3 @@
 export ALIAS_PATH="https://raw.githubusercontent.com/fcenobi/script/master/etc/profile.d/aliases.sh"
-alias updatealias='/bin/bash < <(curl -s $ALIAS_PATH) '
-/bin/bash < <(curl -s $ALIAS_PATH) 
+wget -q $ALIAS_PATH -O   ~/.bash_aliases && source   ~/.bash_aliases
+
