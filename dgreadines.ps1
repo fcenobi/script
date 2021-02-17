491ceebcf0150c934581b72282086cd46c37d38a
Filippo Cenobi <11414315+fcenobi@users.noreply.github.com>
2021-02-17 15:46:44 +0100

    Create dgreadines.ps1

---
+++ dgreadines.ps1
@@ -0,0 +1,7 @@
+## wget https://download.microsoft.com/download/B/D/8/BD821B1F-05F2-4A7E-AA03-DF6C4F687B07/dgreadiness_v3.6.zip
+## unzip dgreadiness_v3.6.zip
+Set-ExecutionPolicy Unrestricted -force
+cd dgreadiness_v3.6
+.\DG_Readiness_Tool_v3.6.ps1 -Capable
+.\DG_Readiness_Tool_v3.6.ps1 -Enable .\DefaultWindows_Audit_sipolicy.p7b
+.\DG_Readiness_Tool_v3.6.ps1 -Disable dg
