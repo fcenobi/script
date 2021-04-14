ce69079c35ecbadb7b360c41c3e712706862c550
Filippo Cenobi <11414315+fcenobi@users.noreply.github.com>
2021-04-14 21:36:24 +0200

    Create sedappendtoip.sh

---
+++ sedappendtoip.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#for variable
+
+#echo $1 | sed 's/\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}/<tring-to-append>&/gp'
+
+#for file 
+
+#sed 's/\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}/<tring-to-append>&/gp' $1
