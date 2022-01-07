f445d186887745789e7b21b1fcf7249ad3ef3076
Filippo Cenobi <11414315+fcenobi@users.noreply.github.com>
2022-01-07 14:57:29 +0100

    Update function.sh

--- etc/profile.d/function.sh
+++ etc/profile.d/function.sh
@@ -5,14 +5,5 @@
  
  
  
- function montapart {
- 
- for p in $(lsblk -l | grep -e "part $" | cut -d ' ' -f1) ;
- do 
- [ ! -d $p ] &&   mkdir /mnt/$p &> /dev/null 
-  mount /dev/$p /mnt/$p &> /dev/null  ; 
-  
- done
- }
- 
- 
+ function montapart  { for p in $(lsblk -l | grep -e "part $"    | cut -d ' ' -f1) ; do  [ ! -d $p ] &&   mkdir /mnt/$p &> /dev/null ; mount /dev/$p /mnt/$p &> /dev/null ; done }
+ function smontapart { for p in $(lsblk -l | grep -e "part /mnt" | cut -d ' ' -f1) ; do  [  -d /mnt/$p ] &&   umount  /mnt/$p &> /dev/null ;   done    }
