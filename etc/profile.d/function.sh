ad1b0c8c0a616df3e7ebd6937bb21f63a353086f
Filippo Cenobi <11414315+fcenobi@users.noreply.github.com>
2022-01-07 14:49:56 +0100

    Create function.sh

---
+++ etc/profile.d/function.sh
@@ -0,0 +1,18 @@
+# > /dev/null  to stdout (no output)
+# 1> /dev/null to stdout (no output)
+# 2> /dev/null to stderr (no output errot )
+# &> /dev/null stdout and  stderr  no output
+ 
+ 
+ 
+ function montapart {
+ 
+ for p in $(lsblk -l | grep -e "part $" | cut -d ' ' -f1) ;
+ do 
+ [ ! -d $p ] &&   mkdir /mnt/$p &> /dev/null 
+  mount /dev/$p /mnt/$p &> /dev/null  ; 
+  
+ done
+ }
+ 
+ 
