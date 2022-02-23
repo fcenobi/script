4fd10e3211c5106c4e987bb175802d55e5b526ae
Filippo Cenobi <11414315+fcenobi@users.noreply.github.com>
2022-02-23 01:12:09 +0100

    Update setjavahome.sh

--- usr/local/sbin/setjavahome.sh
+++ usr/local/sbin/setjavahome.sh
@@ -1,1 +1,3 @@
-  cat /usr/lib/jvm/.*info | grep '^hl java' | sed 's/\/bin\/java//g' | sed 's/hl java /#JAVA_HOME=/g' 
+JAVA_HOMES=$(cat /usr/lib/jvm/.*info | grep '^hl java' | sed 's/\/bin\/java//g' | sed 's/hl java /JAVA_HOME=/g' )
+for i in $JAVA_HOMES ; do echo export $i ;done
+echo 'export PATH=$JAVA_HOME/bin:$PATH' 
