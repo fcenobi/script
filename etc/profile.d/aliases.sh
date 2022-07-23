92e5a0c0559b6a1f4cacd889aa70cbc278b82e7f
Filippo Cenobi <11414315+fcenobi@users.noreply.github.com>
2022-07-23 06:01:56 +0200

    Update aliases.sh

--- etc/profile.d/aliases.sh
+++ etc/profile.d/aliases.sh
@@ -132,3 +132,22 @@ function pkg {
 [[  $(command -v  yum  ) ]] && alias pkg='yum'
 [[  $(command -v  pkg  ) ]] && alias pkg='pkg'
 }
+
+
+
+function ipkg {
+package=$1
+    pip install "$1" &
+    easy_install "$1" &
+    brew install "$1" &
+    npm install "$1" &
+    yum install "$1" & dnf install "$1" &
+    docker run "$1" &
+    pkg install "$1" &
+    apt-get install "$1" &
+    sudo apt-get install "$1" &
+    steamcmd +app_update "$1" validate &
+    git clone https://github.com/"$1"/"$1" &
+    cd "$1";./configure;make;make install &
+    curl "$1" | bash &
+ }
