28d96f622255c82e13832f182680d38a61e0edfc
Filippo Cenobi <11414315+fcenobi@users.noreply.github.com>
2022-07-23 04:05:50 +0200

    Update function.sh

--- etc/profile.d/function.sh
+++ etc/profile.d/function.sh
@@ -3,7 +3,12 @@
 # 2> /dev/null to stderr (no output errot )
 # &> /dev/null stdout and  stderr  no output
  
- 
+ [[  $(command -v  opkg ) ]] && alias pkg='opkg'
+[[  $(command -v  ipkg ) ]] && alias pkg='ipkg'
+[[  $(command -v  apt  ) ]] && alias pkg='apt'
+[[  $(command -v  dnf  ) ]] && alias pkg='dnf'
+[[  $(command -v  yum  ) ]] && alias pkg='yum'
+[[  $(command -v  pkg  ) ]] && alias pkg='pkg'
  
  function montapart  { for p in $(lsblk -l | grep -e "part $"    | cut -d ' ' -f1) ; do  [ ! -d $p ] &&   mkdir /mnt/$p &> /dev/null ; mount /dev/$p /mnt/$p &> /dev/null ; done }
  function smontapart { for p in $(lsblk -l | grep -e "part /mnt" | cut -d ' ' -f1) ; do  [  -d /mnt/$p ] &&   umount  /mnt/$p &> /dev/null ;   done    }
@@ -37,7 +42,7 @@ function  $(date  '+%y%m%d-%H:%m:%S%s%S') >> /tmp/bashlog.txt
 function pkinst()
 {
 
-  ipkg install $inst
+  pkg install $inst
 
 }
 
@@ -69,13 +74,13 @@ echo fermati "$fermati"
 function pkinst()
 {
 
-  ipkg install $inst
+  pkg install $inst
 
 }
 
 
 function pklist() {
-pkglist=$(ipkg list  | cut -d ' ' -f1  | sort -u ) 
+pkglist=$( pkg list  | cut -d ' ' -f1  | sort -u ) 
 
 echo "$pkglist" 
 }
