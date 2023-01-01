95fe1c12b5a9af347f3fa110b9a659d62e6edd2d
Filippo Cenobi <11414315+fcenobi@users.noreply.github.com>
2023-01-01 23:25:22 +0100

    Create navalias.cmd

---
+++ doskey/navalias.cmd
@@ -0,0 +1,20 @@
+::::::::::::::::::::::::::::::::::::::::::::::
+:: Navigation aliasI                        ::
+::::::::::::::::::::::::::::::::::::::::::::::
+:navigation
+@DOSKEY ls                     =dir /w
+@DOSKEY clear                  =cls
+@DOSKEY system32               =cd C:\Windows\System32
+@DOSKEY ..                     =cd ..\$*
+@DOSKEY ...                    =cd ..\..\$*
+@DOSKEY ....                   =cd ..\..\..\$*
+@DOSKEY .....                  =cd ..\..\..\..\$*
+@DOSKEY history                =doskey /history
+@DOSKEY kill                   =taskkill /PID $*
+@DOSKEY ls                     =dir $*
+@DOSKEY mv                     =move $*
+@DOSKEY restex                 =taskkill /f /im explorer.exe$T explorer.exe 
+@DOSKEY exphere                =explorer.exe . 
+@DOSKEY sudo                   =runas /user:administrator $*
+@DOSKEY list                   =cmd   /v:on /c "for %%p in ( "!path:;=" "!" ) do @echo %%~p"
+@DOSKEY end                    =doskey /history >> %USERPROFILE%\history.txt
