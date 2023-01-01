77dfb857556c3aa480d425dcb4c0ebf245eb8fb2
Filippo Cenobi <11414315+fcenobi@users.noreply.github.com>
2023-01-01 22:20:06 +0100

    Create ifolderalias.cmd

---
+++ doskey/ifolderalias.cmd
@@ -0,0 +1,30 @@
+::::::::::::::::::::::::::::::::::::::::::::::
+:: Personal folder I                        ::
+::::::::::::::::::::::::::::::::::::::::::::::
+:i
+@DOSKEY homes                  =cd /d i:\
+@DOSKEY E                      =cd /d i:\E
+@DOSKEY pxe                    =cd /d i:\pxe
+@DOSKEY varie                  =cd /d i:\varie
+@DOSKEY web                    =cd /d i:\web
+@DOSKEY downloads              =cd /d i:\downloads
+:idocument
+@DOSKEY documents              =cd /d i:\Documents
+:idocumentpwsh
+@DOSKEY pwshhome               =cd /d i:\Documents\powershell
+@DOSKEY pwshfunc               =cd /d i:\Documents\powershell\function.d
+@DOSKEY pwshsrc                =cd /d i:\Documents\powershell\script
+@DOSKEY mydoc                  =cd /d i:\Documents\mydoc
+:imoba
+@DOSKEY moba                   =cd /d i:\mobaxterm
+@DOSKEY mobahome               =cd /d i:\mobaxterm\home
+:iprograms
+@DOSKEY programs               =cd /d i:\programs
+@DOSKEY tools                  =cd /d i:\programs\tools
+@DOSKEY sysinternal            =cd /d I:\programs\tools\SysinternalsSuite
+@DOSKEY installer              =cd /d I:\programs\tools\installer
+@DOSKEY portableapps           =cd /d I:\programs\portableapps
+:irepo
+@DOSKEY repo                   =cd /d i:\repo
+@DOSKEY gosetting              =cd /d i:\repo\setting
+::FINEFILE
